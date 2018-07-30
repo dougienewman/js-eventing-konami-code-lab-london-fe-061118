1174905a5f81db1fe02b724caba575543425deb4
Doug Newman <dougnewman@gmail.com>
2018-07-30 18:29:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 console.log(code)
 
-function e() {
+function init(e) {
   // Write your JavaScript code inside the init() function
 
 const body = document.querySelector("body")
@@ -23,4 +23,4 @@ let index = 0;
 console.log(body)
 }
 
-e()
+init()
