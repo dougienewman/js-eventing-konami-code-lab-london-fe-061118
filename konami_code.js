75d638515025dc11b642e3e154b4b0e4a2d03c9f
Doug Newman <dougnewman@gmail.com>
2018-07-30 18:05:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,37 +17,9 @@ console.log(body)
 init()
 
 
+
+
 const konamicode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 console.log(konamicode)
-
-let index = 0;
-
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
- 
-  if (key === alphabet[index]) {
-    index++;
- 
-    if (index === alphabet.length) {
-      alert("Hurray!");
- 
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
-
-console.log(code)
-
-
-
-
-
-
-
-
-
-
 
